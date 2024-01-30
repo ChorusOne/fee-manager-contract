645781d69407cd620422eed14f6b66c2674cace3
Fynn <me@enriquefynn.com>
2024-01-30 18:04:26 -0300

    Add script to deploy contract

---
+++ script/FeeRewardsManager.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/FeeRewardsManager.sol";
+
+contract Deployer is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        new FeeRewardsManager(uint32(vm.envUint("DEFAULT_FEE")));
+        vm.stopBroadcast();
+    }
+}
